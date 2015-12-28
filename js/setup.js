a5c62aad32c86192dc82aa3e9f550917d83607df
Akshay Bairagi <bairagiakshay@gmail.com>
2015-12-28 13:39:37 +0530

    last upd time
    update in pause Game

--- js/setup.js
+++ js/setup.js
@@ -223,6 +223,7 @@ function keyHandler(){
 	//pause function for stopping the game
 	function pauseGame(){
 		if(g.paused && ai.state == 'pause'){
+			ai.t0 = Date.now();
 			g.resume();
 			toggleMenu(pauseScene,undefined);
 			sBox.restart(sBox.bgMusic);
@@ -1069,6 +1070,7 @@ function PauseScene(){
 	o.div.onclick = function(){
 		toggleMenu(o,undefined);
 		// focusText.focus();
+		ai.t0 = Date.now();
 		g.resume();
 		sBox.restart(sBox.bgMusic);
 		player.walk();
